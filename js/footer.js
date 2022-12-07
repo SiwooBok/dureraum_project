a96559b2ad907f9d44a360cacaf8d1e1fec60541
Ian-Woods <woodsw2021@gmail.com>
2022-12-08 01:24:33 +0900

    third_upload

--- js/footer.js
+++ js/footer.js
@@ -1,3 +1,5 @@
+
+
 // -------- related_institution --------
 let footerRelatedSlide = new Swiper(".related_institution_sheet", {
   loop: true,
@@ -40,6 +42,45 @@ let footerMecenatSlide = new Swiper(".mecenat_sheet", {
   }
 });
 
+// -------- search_recommend_sheet --------
+const searchRecommendSheet = document.querySelector('.fast_reserve .select_theater .search_recommend_sheet');
+let searchRecommendSlide = new Swiper(searchRecommendSheet, {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 30,
+  // autoplay: {
+  //   delay: 4000,
+  //   disableOnInteraction: false,
+  // },
+  // breakpoints: {
+  //   320 : {
+  //     slidesPerView: 2,
+  //   },
+  //   1360 : {
+  //     slidesPerView: 3,
+  //   }
+  // }
+});
+
+// -------- search_schedule_sheet --------
+const theaterScheduleSheet = document.querySelector('.fast_reserve .select_theater .detail_info .theater_schedule_sheet');
+let theaterScheduleSlide = new Swiper(theaterScheduleSheet, {
+  // loop: true,
+  slidesPerView: 1,
+  // spaceBetween: 30,
+  // autoplay: {
+  //   delay: 4000,
+  //   disableOnInteraction: false,
+  // },
+  // breakpoints: {
+  //   320 : {
+  //     slidesPerView: 2,
+  //   },
+  //   1360 : {
+  //     slidesPerView: 3,
+  //   }
+  // }
+});
 
 // 스크롤시 퀵메뉴 보이게 하는 섹션
 const quickMenu = document.querySelector('.quick_menu');
@@ -58,9 +99,10 @@ window.addEventListener('scroll', _.throttle(function () {
   }
 }, 300));
 
-
+// ↓↓↓↓↓↓↓↓ jQuery ↓↓↓↓↓↓↓↓
 $(function(){
 
+  // -------- 퀵메뉴 버튼 기능 --------
   $('.btn_to_top').click(function(e){
     e.preventDefault;
     $(window).scrollTo(this.hash || 0, 1000);
@@ -71,4 +113,63 @@ $(function(){
     $(window).scrollTo(this.hash || 5000, 1000);
   });
 
+  const selectTheater = $('.fast_reserve .fast_reserve_contents .fast_reserve_sheet_box .select_theater');
+  const selectSeats = $('.fast_reserve .fast_reserve_contents .fast_reserve_sheet_box .select_seats');
+  const selectPay = $('.fast_reserve .fast_reserve_contents .fast_reserve_sheet_box .pay');
+
+  const btnSearch = $('.fast_reserve .fast_reserve_contents .fast_reserve_sheet_box .select_theater .search_icon');
+
+  const btnNextInTheater = $('.fast_reserve .fast_reserve_contents .fast_reserve_sheet_box .select_theater .btn_next');
+  const btnNextInSeats = $('.fast_reserve .fast_reserve_contents .fast_reserve_sheet_box .select_seats .btn_next');
+  const btnNextInPay = $('.fast_reserve .fast_reserve_contents .fast_reserve_sheet_box .pay .btn_next');
+  const btnPreviousInSeats = $('.fast_reserve .fast_reserve_contents .fast_reserve_sheet_box .select_seats .btn_previous');
+  const btnPreviousInPay = $('.fast_reserve .fast_reserve_contents .fast_reserve_sheet_box .pay .btn_previous');
+
+  const paginationSeats = $('.fast_reserve .fast_reserve_contents .pagination_seats');
+  const paginationPay = $('.fast_reserve .fast_reserve_contents .pagination_pay');
+
+  const selectSeatsDisplay = $('.fast_reserve .fast_reserve_contents .fast_reserve_sheet_box .select_seats_box .select_seat_display');
+
+  // --------  빠른예매 섹션 --------
+  btnNextInTheater.click(function() {
+    selectTheater.css('display', 'none');
+    selectSeats.css('display', 'block')
+    paginationSeats.addClass('proceeding')
+  })
+
+  btnNextInSeats.click(function() {
+    selectSeats.css('display', 'none');
+    selectPay.css('display', 'block')
+    paginationPay.addClass('proceeding')
+  })
+
+  btnPreviousInSeats.click(function() {
+    selectSeats.css('display', 'none');
+    selectTheater.css('display', 'block')
+    paginationSeats.removeClass('proceeding')
+  })
+
+  btnPreviousInPay.click(function() {
+    selectPay.css('display', 'none');
+    selectSeats.css('display', 'block')
+    paginationPay.removeClass('proceeding')
+  })
+
+  btnNextInPay.click(function() {
+    alert('사이트 점검중입니다. 나중에 다시 시도하세요.')
+  })
+
+  btnSearch.click(function() {
+    alert('사이트 점검중입니다. 추천 작품에서 선택해주세요.')
+  })
+
+  // -------- 빠른예매 내부 페이지 이동 정의 --------
+  // 좌석선택 기능 ( pseudo )
+  selectSeatsDisplay.click(function() {
+    selectSeats.addClass('chose');
+  })
+
+
+
 });
+// ↑↑↑↑↑↑↑↑ jQuery ↑↑↑↑↑↑↑↑
